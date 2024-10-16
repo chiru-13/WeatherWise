a8cb5d074e3d75f202849781e7f7a7b1fe231ac0
R Chiranjeevi Srinivas <121923468+chiru-13@users.noreply.github.com>
2024-10-16 11:28:01 +0530

    Update navbar.jsx

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -8,7 +8,7 @@ export function Navbar() {
     <nav className="navbar">
       <div className="climate">
         <img className="logo" src={logo} alt="logo" height={80} />
-        <h1>CliMate</h1>
+        <h1>WeatherWise</h1>
       </div>
       <img className="history-icon" src={historyIcon} alt="history" height={40} />
     </nav>
